9ceeaa3214cebeb001ca45f9ea62e91863ef3e85
pshow <pshow@DESKTOP-9U9F4AS>
2023-09-11 15:44:13 +0900

    1234

--- Chapter01_project1/src/Main_Class.java
+++ Chapter01_project1/src/Main_Class.java
@@ -3,7 +3,10 @@ public class Main_Class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("GitHub...");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
 
 }
