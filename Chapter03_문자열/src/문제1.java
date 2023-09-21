b4a637a62ef4a86a6258bb58b94ae5ea1cb8324b
pshow <pshow@DESKTOP-9U9F4AS>
2023-09-21 17:49:03 +0900

    20230921

--- Chapter03_문자열/src/문제1.java
+++ Chapter03_문자열/src/문제1.java
@@ -1,15 +1,69 @@
 import java.util.Scanner;
 /*
-
+ *  =>조건문
+ *    단일 조건문 / 선택조건문 / 다중조건문 => 형식 , 순서
+ *    ----------------------------
+ *    연산자 => 어떤 연산자 => 결과값?
+ *    ------- 비교연산자 , 논리연산자 , 부정연산자
+ *    단일조건문
+ *    -------
+ *      if(조건문) => 오류(예외처리) => 사전에 오류방지
+ *      {
+ *        조건이 true일 때만 수행
+ *      }
+ *      => 수행 => 건너뛴다
+ *      
+ *    선택조건문 => 삼항연산자
+ *    -------
+ *      if(조건문)
+ *      {
+ *          //사용자
+ *      }
+ *      else
+ *      {
+ *          //관리자
+ *      }
+ *      
+ *      다중조건문 => 조건에 맞는 문장만 수행 ==> switch~case
+ *      -------
+ *      if(조건문)
+ *      {
+ *        조건이 true일 때 => 수행 => 종료
+ *        false일 때 밑의 조건문을 찾는다.
+ *      }
+ *      else if(조건문)
+ *      {
+ *        조건이 true일 때 => 수행 => 종료
+ *        false일 때 밑의 조건문을 찾는다.
+ *      }
+ *      else
+ *      {
+ *        해당 조건에 맞는 경우 처리
+ *      }
+ *      =======================
+ *      선택문
+ *      switch(정수,문자,문자열){ => 실수 미포함
+ *          case 값:
+ *             처리 문장
+ *             break;
+ *          default: ==> 생략가능
+ *             처리 문장
+ *      }
+ *    
  * */
 public class 문제1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.print("정수입력(10~99) : ");
-		int num = sc.nextInt();
-		
+		// enter, space 
+		int num = 0;
+		while(true) {
+			System.out.print("정수입력(10~99) : ");
+			num = sc.nextInt();
+			if(num<10||num>99) continue;
+			break;
+		}
 		if((num%10)==(num/10)) System.out.println("10의 자리와 1의 자리가 같습니다.");
 		else System.out.println("10의 자리와 1의 자리가 다릅니다.");
 
--- Chapter03_문자열/src/문제2.java
+++ Chapter03_문자열/src/문제2.java
@@ -1,5 +1,7 @@
 /*
  * 3~5는 봄 6~8여름 9~11 가을 12~2 겨울
+ * 
+ * 다중 조건문 / 선택문
  * */
 import java.util.Scanner;
 public class 문제2 {
@@ -8,13 +10,34 @@ public class 문제2 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("달을 입력하세요(1~12) : ");
-		int month = sc.nextInt();
-		
+		int month = 0;
+		while(true) {
+			System.out.print("달을 입력하세요(1~12) : ");
+			month = sc.nextInt();
+			if(month<1||month>12) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
+			break;
+		}
 		if(month>=3&&month<=5) System.out.println("봄");
 		else if(month>=6&&month<=8) System.out.println("여름");
 		else if(month>=9&&month<=11) System.out.println("가을");
 		else System.out.println("겨울");
+		
+		switch(month) {
+			case 12:case 1:case 2:
+				System.out.println("겨울");
+				break;
+			case 3:case 4:case 5:
+				System.out.println("봄");
+				break;
+			case 6:case 7:case 8:
+				System.out.println("여름");
+				break;
+			default:
+				System.out.println("가을");
+		}
 
 	}
 
