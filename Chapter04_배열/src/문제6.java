ae28b415ec1ffdf10057450644dfa915418af9d4
pshow <pshow@DESKTOP-9U9F4AS>
2023-09-25 08:54:07 +0900

    20230925

--- Chapter04_배열/src/문제6.java
+++ Chapter04_배열/src/문제6.java
@@ -4,15 +4,15 @@ public class 문제6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Scanner sc = new Scanner(System.in);
-//		int[] arr = new int[10];
-//		int sum=0;
-//		double avg;
-//		for(int i =0;i<arr.length;i++) {
-//			arr[i] = sc.nextInt();
-//			sum+=arr[i];
-//		}
-//		System.out.printf("총점 : %d , 평균 : %f",sum,sum/(double)arr.length);
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		int sum=0;
+		double avg;
+		for(int i =0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+			sum+=arr[i];
+		}
+		System.out.printf("총점 : %d , 평균 : %f",sum,sum/(double)arr.length);
 //		
 		int[] i_arr = {95,75,85,100,50};
 		int temp = 0;
