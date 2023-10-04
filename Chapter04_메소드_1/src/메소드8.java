1678a2f43bbc846135429be8c16b98f7fa55f7fc
pshow <pshow@DESKTOP-9U9F4AS>
2023-10-04 12:49:50 +0900

    20231004

---
+++ Chapter04_메소드_1/src/메소드8.java
@@ -0,0 +1,19 @@
+/*
+ * increment() => num : 1
+ * increment() => num : 1
+ * increment() => num : 1
+ * */
+public class 메소드8 {
+	public static void increment() {
+		int num = 1;
+		System.out.println(num);
+		num++;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
