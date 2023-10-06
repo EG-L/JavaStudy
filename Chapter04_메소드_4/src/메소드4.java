748e80c187b583fcfac8f18bab593dac55e11272
pshow <pshow@DESKTOP-9U9F4AS>
2023-10-06 12:51:24 +0900

    20231006

---
+++ Chapter04_메소드_4/src/메소드4.java
@@ -0,0 +1,16 @@
+/*
+ *    달력 만들기
+ *    ---------
+ *     절차적 언어 ==> 구조적 프로그램 == 객체지향 프로그램
+ *                 => 메소드        => 메소드
+ *                 => 지역변수       => 전역변수
+ *               매개변수 전송, 리턴형
+ * */
+public class 메소드4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
