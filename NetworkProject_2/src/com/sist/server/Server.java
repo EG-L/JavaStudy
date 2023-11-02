ba8b710ec4239e43197b06680e91094c03851b76
pshow <pshow@DESKTOP-9U9F4AS>
2023-11-02 15:27:00 +0900

    20231102

--- NetworkProject_2/src/com/sist/server/Server.java
+++ NetworkProject_2/src/com/sist/server/Server.java
@@ -1,6 +1,48 @@
 package com.sist.server;
 
-public class Server {
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Server implements Runnable{
+	private Vector<Client> waitVc = new Vector<Client>();
+	private ServerSocket ss;
+	private final int PORT = 8080;
 	
+	public Server(){
+		try {
+			ss = new ServerSocket(PORT);
+			
+			System.out.println("접속 대기 중..");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			while(true){
+				Socket s = ss.accept();
+				
+				Client client = new Client(s);
+				client.start();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	class Client extends Thread{
+		String id,name,sex,pos;
+		Socket s;
+		
+		public Client(Socket s) {
+			this.s = s;
+			
+		}
+	}
 
 }
